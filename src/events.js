bafddb3cb6a994f2b27e72d0693e1bce363c9dff
T Gubs <T Gubs>
2017-08-14 00:16:19 -0400

    Added the binding to prevent context loss, propert key checking

--- src/events.js
+++ src/events.js
@@ -4,8 +4,8 @@
         class Factory{
             constructor(){
                 const Factory = (NodeEvents ? new NodeEvents() : document.createDocumentFragment());
-                this.addEventListener = this.addListener = this.on = Factory.on || Factory.addEventListener;
-                this.removeEventListener = this.removeListener = Factory.removeListener || Factory.removeEventListener;
+                this.addEventListener = this.addListener = this.on = ("on" in Factory? Factory.on.bind(Factory): Factory.addEventListener.bind(Factory));
+                this.removeEventListener = this.removeListener = ("removeListener" in Factory? Factory.removeListener.bind(Factory): Factory.removeEventListener.bind(Factory));
                 this.dispatchEvent = this.emit = function(){
                     if(typeof CustomEvent === 'undefined'){
                         Factory.emit(...arguments);
@@ -14,13 +14,13 @@
                         Factory.dispatchEvent(new CustomEvent(...arguments));
                     }
                 }
-                this.once = Factory.once || function(event, func){
+                this.once = ("once" in Factory? Factory.once.bind(Factory): function(event, func){
                     const onceFunction = function(){
                         func();
                         this.removeEventListener(event, onceFunction);
                     }
                     this.addEventListener(event, onceFunction);
-                };
+                });
             }
         }
         exports.Factory = Factory;
